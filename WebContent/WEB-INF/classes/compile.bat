57d514b41fb632575ec7036785ec0393d3afc0f4
KeeperXTL <sergosergo0001@gmail.com>
2020-05-11 21:51:57 +0300

    retry

--- WebContent/WEB-INF/classes/compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-color 0a
-javac -classpath "C:\Program Files\Apache Software Foundation\Tomcat 9.0\lib\servlet-api.jar" -d bin -sourcepath ..\..\..\src *.java
-pause
--- WebContent/js/singUp.js
+++
@@ -1,9 +0,0 @@
-function singUpShow() {
-	var singUp = document.querySelector("#singUp");
-	singUp.show();
-}
-
-function singUpClose() {
-	var singUp = document.querySelector("#singUp");
-	singUp.close();
-}
